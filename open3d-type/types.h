964fa7ec0629fcc1a6cc3cc3d1469ee4f000f026
takanokage <danut.petre@gmail.com>
2019-03-03 10:31:42 -0800

    comments. cast operators made explicit.

--- open3d-type/types.h
+++ open3d-type/types.h
@@ -24,15 +24,21 @@ extern "C"
             struct { float x, y, z; };
             struct { float r, g, b; };
 
+            // subscript operators
             float& operator [](const int& i);
             const float& operator [](const int& i) const;
-            operator float* const();
-            operator const float* const();
+
+            // cast operators
+            explicit operator float* const();
+            explicit operator const float* const();
+
+            // comparison operators
             bool operator ==(const _Vector3f& v) const;
             bool operator !=(const _Vector3f& v) const;
             bool operator <=(const _Vector3f& v) const;
             bool operator >=(const _Vector3f& v) const;
 
+            // comparators for use with std::sort
             static bool LEX(const _Vector3f& v0, const _Vector3f& v1);
             static bool GEX(const _Vector3f& v0, const _Vector3f& v1);
             static bool LEY(const _Vector3f& v0, const _Vector3f& v1);
