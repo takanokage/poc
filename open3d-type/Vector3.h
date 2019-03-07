3f3fb9a1f0e65aa8424fdbe9b111aef0af35d425
takanokage <danut.petre@gmail.com>
2019-03-07 14:58:35 -0800

    moved all definitions to the header.

--- open3d-type/Vector3.h
+++ open3d-type/Vector3.h
@@ -9,90 +9,200 @@ typedef unsigned int uint;
 
 namespace open3d
 {
-    static const float FLT_THRESHOLD = -1e3;
-    static const float FLT_PRECISION = 3;
-    static const float FLT_WIDTH = 8;
+static const float FLT_THRESHOLD = -1e3;
+static const float FLT_PRECISION = 3;
+static const float FLT_WIDTH = 8;
 
-    static const float DBL_THRESHOLD = -1e6;
-    static const float DBL_PRECISION = 6;
-    static const float DBL_WIDTH = 12;
+static const float DBL_THRESHOLD = -1e6;
+static const float DBL_PRECISION = 6;
+static const float DBL_WIDTH = 12;
 
-    template<typename T>
-    struct Vector3
-    {
-        typedef union alignas(4 * sizeof(T)) _Type
+template <typename T>
+struct Vector3
+{
+    typedef union alignas(4 * sizeof(T)) _Type {
+        // data
+        struct { T x, y, z; };
+        struct { T r, g, b; };
+
+        // subscript operator: readwrite
+        T &operator[](const uint &i)
         {
-            // data
-            struct { T x, y, z; };
-            struct { T r, g, b; };
+            // catch error in debug mode
+            assert(i < 3);
 
-            // subscript operator: readwrite
-            T& operator [](const uint& i);
-            // subscript operator: readonly
-            const T& operator [](const uint& i) const;
+            return ((T *)this)[i];
+        }
+        // subscript operator: readonly
+        const T &operator[](const uint &i) const
+        {
+            // catch error in debug mode
+            assert(i < 3);
 
-            // casting operator: readwrite
-            explicit operator T* const();
-            // casting operator: readonly
-            explicit operator const T* const();
+            return ((const T *const)this)[i];
+        }
+        // casting operator: readwrite
+        operator T *const()
+        {
+            return reinterpret_cast<T *>(this);
+        }
+        // casting operator: readonly
+        operator const T *const()
+        {
+            return reinterpret_cast<const T *const>(this);
+        }
+        bool operator==(const _Type &v) const
+        {
+            return (x == v.x) && (y == v.y) && (z == v.z);
+        }
+        bool operator!=(const _Type &v) const
+        {
+            return !(this->operator==(v));
+        }
+        bool operator<=(const _Type &v) const
+        {
+            return (x <= v.x) && (y <= v.y) && (z <= v.z);
+        }
+        bool operator>=(const _Type &v) const
+        {
+            return (x >= v.x) && (y >= v.y) && (z >= v.z);
+        }
+        // addtion.
+        _Type operator+(const _Type &v) const
+        {
+            Vector3<T>::Type output = {x + v.x, y + v.y, z + v.z};
 
-            bool operator ==(const _Type& v) const;
-            bool operator !=(const _Type& v) const;
-            bool operator <=(const _Type& v) const;
-            bool operator >=(const _Type& v) const;
+            return output;
+        }
+        // subtraction.
+        _Type operator-(const _Type &v) const
+        {
+            Vector3<T>::Type output = {x - v.x, y - v.y, z - v.z};
 
-            // addition
-            _Type operator +(const _Type& v) const;
-            // subtraction
-            _Type operator -(const _Type& v) const;
-            // addition assignment
-            _Type& operator +=(const _Type& v);
-            // subtraction assignment
-            _Type& operator -=(const _Type& v);
-            // addition
-            _Type operator +(const T& v) const;
-            // subtraction
-            _Type operator -(const T& v) const;
-            // multiply with scalar
-            _Type operator *(const T& v) const;
-            // divide by scalar
-            _Type operator /(const T& v) const;
-            // addition assignment
-            _Type& operator +=(const T& v);
-            // subtraction assignment
-            _Type& operator -=(const T& v);
-            // multiplication assignment
-            _Type& operator *=(const T& v);
-            // division assignment
-            _Type& operator /=(const T& v);
+            return output;
+        }
+        // addtion assignment.
+        _Type &operator+=(const _Type &v)
+        {
+            x += v.x;
+            y += v.y;
+            z += v.z;
 
-            // Less than or equal X component.
-            // Comparator for use with std::sort.
-            static bool LEX(const _Type& v0, const _Type& v1);
-            // Greater than or equal X component.
-            // Comparator for use with std::sort.
-            static bool GEX(const _Type& v0, const _Type& v1);
-            // Less than or equal Y component.
-            // Comparator for use with std::sort.
-            static bool LEY(const _Type& v0, const _Type& v1);
-            // Greater than or equal Y component.
-            // Comparator for use with std::sort.
-            static bool GEY(const _Type& v0, const _Type& v1);
-            // Less than or equal Z component.
-            // Comparator for use with std::sort.
-            static bool LEZ(const _Type& v0, const _Type& v1);
-            // Greater than or equal Z component.
-            // Comparator for use with std::sort.
-            static bool GEZ(const _Type& v0, const _Type& v1);
-        } Type;
-    };
+            return *this;
+        }
+        // subtraction assignment.
+        _Type &operator-=(const _Type &v)
+        {
+            x -= v.x;
+            y -= v.y;
+            z -= v.z;
 
-    typedef Vector3<double>::Type Vector3d;
-    typedef Vector3<float>::Type Vector3f;
-    typedef Vector3<int>::Type Vector3i;
+            return *this;
+        }
+        // addtion.
+        _Type operator+(const T &t) const
+        {
+            Vector3<T>::Type output = {x + t, y + t, z + t};
 
-    // Display.
-    std::ostream& operator <<(std::ostream& os, const Vector3d& v);
-    std::ostream& operator <<(std::ostream& os, const Vector3f& v);
-    std::ostream& operator <<(std::ostream& os, const Vector3i& v);
-}
+            return output;
+        }
+        // subtraction.
+        _Type operator-(const T &t) const
+        {
+            Vector3<T>::Type output = {x - t, y - t, z - t};
+
+            return output;
+        }
+        // multiply with scalar.
+        _Type operator*(const T &t) const
+        {
+            Vector3<T>::Type output = {x * t, y * t, z * t};
+
+            return output;
+        }
+        // divide by scalar.
+        _Type operator/(const T &t) const
+        {
+            Vector3<T>::Type output = {x / t, y / t, z / t};
+
+            return output;
+        }
+        // addtion assignment.
+        _Type &operator+=(const T &t)
+        {
+            x += t;
+            y += t;
+            z += t;
+
+            return *this;
+        }
+        // subtraction assignment.
+        _Type &operator-=(const T &t)
+        {
+            x -= t;
+            y -= t;
+            z -= t;
+
+            return *this;
+        }
+        // multiplication assignment.
+        _Type &operator*=(const T &t)
+        {
+            x *= t;
+            y *= t;
+            z *= t;
+
+            return *this;
+        }
+        // division assignment.
+        _Type &operator/=(const T &t)
+        {
+            x /= t;
+            y /= t;
+            z /= t;
+
+            return *this;
+        }
+        // Less than or equal X component.
+        static bool LEX(const _Type &v0, const _Type &v1)
+        {
+            return v0.x <= v1.x;
+        }
+        // Greater than or equal X component.
+        static bool GEX(const _Type &v0, const _Type &v1)
+        {
+            return v0.x >= v1.x;
+        }
+        // Less than or equal Y component.
+        static bool LEY(const _Type &v0, const _Type &v1)
+        {
+            return v0.y <= v1.y;
+        }
+        // Greater than or equal Y component.
+        static bool GEY(const _Type &v0, const _Type &v1)
+        {
+            return v0.y >= v1.y;
+        }
+        // Less than or equal Z component.
+        static bool LEZ(const _Type &v0, const _Type &v1)
+        {
+            return v0.z <= v1.z;
+        }
+        // Greater than or equal Z component.
+        static bool GEZ(const _Type &v0, const _Type &v1)
+        {
+            return v0.z >= v1.z;
+        }
+
+    } Type;
+};
+
+typedef Vector3<double>::Type Vector3d;
+typedef Vector3<float>::Type Vector3f;
+typedef Vector3<int>::Type Vector3i;
+
+// Display.
+std::ostream &operator<<(std::ostream &os, const Vector3d &v);
+std::ostream &operator<<(std::ostream &os, const Vector3f &v);
+std::ostream &operator<<(std::ostream &os, const Vector3i &v);
+} // namespace open3d
